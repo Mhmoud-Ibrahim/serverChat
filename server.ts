1cdd914145eb9fbfc1dc3f714f7264dac2c0297c
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-07 03:16:02 +0400

    sory

--- server.ts
+++ server.ts
@@ -1,31 +1,20 @@
+
+
 import http from 'http';
 import { Server } from 'socket.io';
 import express from 'express';
 import type { Application, NextFunction, Request, Response } from 'express';
 import cookieParser from 'cookie-parser'; 
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url'; // لإصلاح مسار الملفات
-
-// استيراد الملفات الخاصة بالمشروع
-
+import { registerChatHandlers } from './src/modules/socket/chatHandler.js';
 import { AppError } from './src/utils/appError.js';
 import globalErrorHandler from './src/middleware/globalError.js';
+import cors from 'cors';
 import userRouter from './src/modules/user/user.routes.js';
 import imgRouter from './src/modules/imageHandler/imgs.routes.js';
 import socketAuth from './src/middleware/socket.js';
 import { dbConnections } from './db/dbConnections.js';
-import { registerChatHandlers } from './src/modules/socket/chatHandler.js';
-
-// تشغيل اتصال قاعدة البيانات
-await dbConnections();
-
+dbConnections();
 const app: Application = express();
-
-// إعدادات المسارات لـ ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
@@ -34,24 +23,23 @@ app.use(cors({
   credentials: true, 
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://chatnow26.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    credentials: true, // ضروري للسماح بالكوكيز
     allowedHeaders: ['Content-Type', 'Authorization']
-  },allowEIO3: true
+  }
 });
 
-// المسارات (Routes)
+import path from 'path';
 app.use('/auth', userRouter);
 app.use(imgRouter);
-app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
-// إعدادات Socket.io
- io.use(socketAuth);
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+io.use(socketAuth);
 io.on('connection', (socket) => {
   console.log(`🚀 User Authenticated: ${socket.data.userId} (Socket ID: ${socket.id})`);
   registerChatHandlers(io, socket);
@@ -60,17 +48,12 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/', (req, res) => res.send('OK its working .....'));
-
-// معالجة المسارات غير الموجودة
+app.get('/', (req, res) => res.send('OK'));
 app.all(/(.*)/, (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Route ${req.originalUrl} Not Found`, 404))
-});
-
-// معالج الأخطاء العام
-app.use(globalErrorHandler);
-
-const port = process.env.PORT || 7860;
+})
+app.use(globalErrorHandler)
+const port =process.env.PORT || 7860;
 server.listen(port, () => {
   console.log(` Server running on http://localhost:${port}`);
 });
